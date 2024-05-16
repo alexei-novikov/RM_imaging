976cbfc6035388f88f14273fd7901c898136ef14
Alex Christie <acc5843@stanford.edu>
2024-05-16 10:16:13 -0700

    modified:   CCSweep.py

--- CCSweep.py
+++ CCSweep.py
@@ -41,14 +41,14 @@ parameters_dict = {
         'values' :[[500,500, 500, 300, 300, 400,800]],#[1000,950, 900, 850, 800, 400, 400, 400, 400, 800, 1000]]#[[encoder], [decoder]] 
     },          
     'dropout':{'values':[.25]}, #default 0
-    'num_epochs' :{ 'values': [1000] },#number of data passes
+    'num_epochs' :{ 'values': [3000] },#number of data passes
     'seed':{'values':[0]},
     'data_type': {'values': ['PNAS-regime_all_seeds']},#'p1_40real_6e-1_sameconfigurations_all_seeds','p1_400real_6e-1_sameconfigurations_all_seeds','p1_4000real_6e-1_sameconfigurations_all_seeds','p1_20real_6e-1_singletarg_all_seeds','p1_40real_6e-1_singletarg_all_seeds','p1_400real_6e-1_singletarg_all_seeds','p1_4000real_6e-1_singletarg_all_seeds']},#'p1_IIDreal_6e-1_all_seeds','p1_400real_6e-1_all_seeds','p1_4000real_6e-1_all_seeds', ,'p1_20real_6e-1_all_seeds'
     'net_type': {'values':['NL_L']},#model type.'fc_NC', recurrent_out' 'fc', 'linear', 'conv' (conv has not been updated since 10/10/2023
     'labeled_data': {'values': [0]},#what amountof the data to use
-    'unlabeled_data' : {'values' :[320000]},#what amount of the data to use
+    'unlabeled_data' : {'values' :[20000]},#what amount of the data to use
     'Data_locat':{'values':['GELMA_Long']},#where the data is located
-    'l1_weight' :{'values': [5e-15]},
+    'l1_weight' :{'values': [1.0]},
     'out_encoder':{'values':['sigmoid']},#Sigmoid
     'L1_rescaling':{'values':[False]},
     'L1_burn_time':{'values':[10]},
@@ -62,7 +62,7 @@ parameters_dict = {
     'batch_size':{'values':[128]},
     'lin_type_decoder':{'values':['real']},#real, complex
     'root_MSE':{'values':[True]},
-    'GELMA':{'values':[5e-10]},
+    'GELMA':{'values':[1.0]},
 }
 
 #N alpha/B should be constatnt
