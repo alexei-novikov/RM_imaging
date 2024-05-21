0e7090b0c080e7e429d7d45cfb3682085cd30534
Alex Christie <acc5843@stanford.edu>
2024-05-20 20:21:51 -0700

    modified:   Cleancoder.py 	modified:   Helpers.py 	modified:   multi_dict.ipynb

--- CCSweep.py
+++ CCSweep.py
@@ -41,19 +41,19 @@ parameters_dict = {
         'values' :[[500,500, 500, 300, 300, 400,800]],#[1000,950, 900, 850, 800, 400, 400, 400, 400, 800, 1000]]#[[encoder], [decoder]] 
     },          
     'dropout':{'values':[.25]}, #default 0
-    'num_epochs' :{ 'values': [3000] },#number of data passes
-    'seed':{'values':[0]},
-    'data_type': {'values': ['PNAS-highcoh_regime_all_seeds','PNAS-lowcoh_regime_all_seeds']},#PNAS-highcoh_regime_all_seeds,PNAS-regime_all_seeds,PNAS-lowcoh_regime_all_seeds,PNAS-regime_all_seeds
+    'num_epochs' :{ 'values': [300] },#number of data passes
+    'seed':{'values':[0,1,2,3]},
+    'data_type': {'values': ['PNAS-regime_all_seeds']},#PNAS-highcoh_regime_all_seeds,PNAS-regime_all_seeds,PNAS-lowcoh_regime_all_seeds,PNAS-regime_all_seeds
     'net_type': {'values':['NL_L']},#model type.'fc_NC', recurrent_out' 'fc', 'linear', 'conv' (conv has not been updated since 10/10/2023
     'labeled_data': {'values': [0]},#what amountof the data to use
-    'unlabeled_data' : {'values' :[160000]},#what amount of the data to use
+    'unlabeled_data' : {'values' :[10000]},#what amount of the data to use
     'Data_locat':{'values':['GELMA_Long']},#where the data is located
-    'l1_weight' :{'values': [5e-5]},
+    'l1_weight' :{'values': [5e-1]},
     'out_encoder':{'values':['sigmoid']},#Sigmoid
     'L1_rescaling':{'values':[False]},
     'L1_burn_time':{'values':[10]},
     'activation':{'values':['relu']},#relu sigmoidnvidi
-    'G_0_intiailization':{'values':[True,False]},
+    'G_0_intiailization':{'values':[True]},
     'Dict_first_epochs':{'values':[0]},
     'weight_decay':{'values':[0.01]},
     'sch':{'values':[False]},#StepLR, MultiStepLR, ExponentialLR, CosineAnnealingLR, ReduceLROnPlateau
@@ -62,7 +62,7 @@ parameters_dict = {
     'batch_size':{'values':[128]},
     'lin_type_decoder':{'values':['real']},#real, complex
     'root_MSE':{'values':[True]},
-    'GELMA':{'values':[5e-5]},
+    'GELMA':{'values':[5e-1]},
     'lr':{'values':[1e-3]},
 }
 
