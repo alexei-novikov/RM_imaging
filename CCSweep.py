a64470cefad14c2965baec10dacf6cc2b1b98441
Alex Christie <acc5843@stanford.edu>
2024-05-21 17:32:33 -0700

    modified:   CCSweep.py 	modified:   Cleancoder.py 	modified:   multi_dict.ipynb

--- CCSweep.py
+++ CCSweep.py
@@ -41,14 +41,14 @@ parameters_dict = {
         'values' :[[500,500, 500, 300, 3000, 400,800]],#[1000,950, 900, 850, 800, 400, 400, 400, 400, 800, 1000]]#[[encoder], [decoder]] 
     },          
     'dropout':{'values':[.25]}, #default 0
-    'num_epochs' :{ 'values': [1500] },#number of data passes
-    'seed':{'values':[0]},
+    'num_epochs' :{ 'values': [1000] },#number of data passes
+    'seed':{'values':[0,1,2,3,4]},
     'data_type': {'values': ['PNAS-regime_all_seeds']},#PNAS-highcoh_regime_all_seeds,PNAS-regime_all_seeds,PNAS-lowcoh_regime_all_seeds,PNAS-regime_all_seeds
     'net_type': {'values':['NL_L']},#model type.'fc_NC', recurrent_out' 'fc', 'linear', 'conv' (conv has not been updated since 10/10/2023
     'labeled_data': {'values': [0]},#what amountof the data to use
-    'unlabeled_data' : {'values' :[5000]},#what amount of the data to use
-    'Data_locat':{'values':['Low_data_GELMA']},#where the data is located
-    'l1_weight' :{'values': [1.0]},
+    'unlabeled_data' : {'values' :[20000]},#what amount of the data to use
+    'Data_locat':{'values':['Init_opt_experiments']},#where the data is located
+    'l1_weight' :{'values': [5e-5]},
     'out_encoder':{'values':['sigmoid']},#Sigmoid
     'L1_rescaling':{'values':[False]},
     'L1_burn_time':{'values':[20]},
@@ -62,13 +62,13 @@ parameters_dict = {
     'batch_size':{'values':[128]},
     'lin_type_decoder':{'values':['real']},#real, complex
     'root_MSE':{'values':[True]},
-    'GELMA':{'values':[5e-7]},
+    'GELMA':{'values':[5e-5]},
     'lr':{'values':[1e-3]},
 }
 
 #N alpha/B should be constatnt
 
-
+#1521006 is the benchmark run pid
 
 
 
