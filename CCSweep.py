a0fefc53bed52ea67a896ae252624c5d36d18590
Alex Christie <acc5843@stanford.edu>
2024-05-27 23:22:05 -0700

    modified:   CCSweep.py 	modified:   CCagents.py

--- CCSweep.py
+++ CCSweep.py
@@ -43,17 +43,17 @@ parameters_dict = {
     'dropout':{'values':[.25]}, #default 0
     'num_epochs' :{ 'values': [3000] },#number of data passes
     'seed':{'values':[0]},
-    'data_type': {'values': ['PNAS-regime_all_seeds']},#PNAS-highcoh_regime_all_seeds,PNAS-regime_all_seeds,PNAS-lowcoh_regime_all_seeds,PNAS-regime_all_seeds
+    'data_type': {'values': ['PNAS-highcoh_regime_all_seeds']},#PNAS-highcoh_regime_all_seeds,PNAS-regime_all_seeds,PNAS-lowcoh_regime_all_seeds,PNAS-regime_all_seeds
     'net_type': {'values':['NL_L']},#model type.'fc_NC', recurrent_out' 'fc', 'linear', 'conv' (conv has not been updated since 10/10/2023
     'labeled_data': {'values': [0]},#what amountof the data to use
-    'unlabeled_data' : {'values' :[160000]},#what amount of the data to use
+    'unlabeled_data' : {'values' :[320000]},#what amount of the data to use
     'Data_locat':{'values':['GELMA_long_bigdata']},#where the data is located
     'l1_weight' :{'values': [5e-5]},
     'out_encoder':{'values':['sigmoid']},#Sigmoid
     'L1_rescaling':{'values':[False]},
     'L1_burn_time':{'values':[20]},
     'activation':{'values':['relu']},#relu sigmoidnvidi
-    'G_0_intiailization':{'values':[False]},
+    'G_0_intiailization':{'values':[False, True]},
     'Dict_first_epochs':{'values':[0]},
     'weight_decay':{'values':[0.01]},
     'sch':{'values':[False]},#StepLR, MultiStepLR, ExponentialLR, CosineAnnealingLR, ReduceLROnPlateau
