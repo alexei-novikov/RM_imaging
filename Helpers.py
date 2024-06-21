d1e3200ddda7e4788eacc961223eefe9fd429122
Alex Christie <acc5843@stanford.edu>
2024-06-21 09:51:05 -0700

    modified:   Helpers.py 	modified:   multi_dict.ipynb

--- Helpers.py
+++ Helpers.py
@@ -749,3 +749,51 @@ def reformat_sweep_for_1_run(param_dict):
 
 def KL_divergence(mean, logvar):
     return -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
+
+
+#The below was written by Mattheiu Blondel from 
+#mblondel/projection_simplex_vectorized.py
+def projection_simplex(V, z=1, axis=None):
+    """
+    Projection of x onto the simplex, scaled by z:
+        P(x; z) = argmin_{y >= 0, sum(y) = z} ||y - x||^2
+    z: float or array
+        If array, len(z) must be compatible with V
+    axis: None or int
+        axis=None: project V by P(V.ravel(); z)
+        axis=1: project each V[i] by P(V[i]; z[i])
+        axis=0: project each V[:, j] by P(V[:, j]; z[j])
+    """
+    if axis == 1:
+        n_features = V.shape[1]
+        U = np.sort(V, axis=1)[:, ::-1]
+        z = np.ones(len(V)) * z
+        cssv = np.cumsum(U, axis=1) - z[:, np.newaxis]
+        ind = np.arange(n_features) + 1
+        cond = U - cssv / ind > 0
+        rho = np.count_nonzero(cond, axis=1)
+        theta = cssv[np.arange(len(V)), rho - 1] / rho
+        return np.maximum(V - theta[:, np.newaxis], 0)
+
+    elif axis == 0:
+        return projection_simplex(V.T, z, axis=1).T
+
+    else:
+        V = V.ravel().reshape(1, -1)
+        return projection_simplex(V, z, axis=1).ravel()
+
+
+def _projection_simplex(v, z=1):
+    """
+    Old implementation for test and benchmark purposes.
+    The arguments v and z should be a vector and a scalar, respectively.
+    """
+    n_features = v.shape[0]
+    u = np.sort(v)[::-1]
+    cssv = np.cumsum(u) - z
+    ind = np.arange(n_features) + 1
+    cond = u - cssv / ind > 0
+    rho = ind[cond][-1]
+    theta = cssv[cond][-1] / float(rho)
+    w = np.maximum(v - theta, 0)
+    return w
